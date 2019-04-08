dac4de02c19929a52b1e0dc33334ccaae5a7505f
leahs.campbell <leahs.campbell@gmail.com>
2019-04-08 22:00:06 +0000

    Updated getImagesLib.js (added 1 line, deleted 1 line).

--- getImagesLib.js
+++ getImagesLib.js
@@ -1024,7 +1024,7 @@ function medoidMosaicMSD(inCollection,medoidIncludeBands) {
   medoid = ee.ImageCollection(medoid)
     .reduce(ee.Reducer.min(bandNames.length().add(1)))
     .select(bandNumbers,bandNames);
-
+  Map.addLayer(medoid.clip(geometry),{min:0,max:0.35,bands:'swir1,nir,red'},'medoid '+year,false)
   return medoid;
 }
 
