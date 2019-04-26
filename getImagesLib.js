5e7bd5040b3d957b1b833c36d8490852e0e45f1a
ian.housman <ian.housman@gmail.com>
2019-04-26 17:12:37 +0000

    Updated getImagesLib.js (added 3 lines, deleted 3 lines).

--- getImagesLib.js
+++ getImagesLib.js
@@ -350,7 +350,7 @@ function getS2(studyArea,startDate,endDate,startJulian,endJulian,resampleMethod)
                       var out = t.copyProperties(img).copyProperties(img,['system:time_start']);
                     return out;
                       })
-                      .select(['QA60', 'B1','B2','B3','B4','B5','B6','B7','B8','B8A', 'B9','B10', 'B11','B12'],['QA60','cb', 'blue', 'green', 'red', 're1','re2','re3','nir', 'nir2', 'waterVapor', 'cirrus','swir1', 'swir2'])
+                      .select(['QA60', 'B1','B2','B3','B4','B5','B6','B7','B8','B8A', 'B9','B10', 'B11','B12'],['QA60','cb', 'blue', 'green', 'red', 're1','re2','re3','nir', 'nir2', 'waterVapor', 'cirrus','swir1', 'swir2']);
                       // .map(function(img){return img.resample('bicubic') }) ;
   
   if(['bilinear','bicubic'].indexOf(resampleMethod) > -1){
