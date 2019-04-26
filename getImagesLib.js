8cc20d30d10900a4094811f52a85e31024a87a2d
ian.housman <ian.housman@gmail.com>
2019-04-26 18:12:33 +0000

    Updated getImagesLib.js (deleted 2 lines).

--- getImagesLib.js
+++ getImagesLib.js
@@ -1911,8 +1911,6 @@ function getLandsatWrapper(studyArea,startYear,endYear,startJulian,endJulian,
   if(outputName === undefined || outputName === null){outputName = 'Landsat-Composite'}
   if(exportPathRoot === undefined || exportPathRoot === null){exportPathRoot = 'users/ianhousman/test'}
   if(crs === undefined || crs === null){crs = 'EPSG:5070'}
-  if(transform === undefined || transform === null){transform = null}
-  if(scale === undefined || scale === null){scale = 30}
   if(resampleMethod === undefined || resampleMethod === null){resampleMethod = 'near'}
   // Prepare dates
   //Wrap the dates if needed
