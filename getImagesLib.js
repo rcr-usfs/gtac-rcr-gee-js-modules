3557f9aee783fb633d1b49bff4724a3d43ee0c4c
iwhousman <iwhousman@gmail.com>
2019-04-29 18:58:12 +0000

    Updated getImagesLib.js (added 1 line, deleted 3 lines).

--- getImagesLib.js
+++ getImagesLib.js
@@ -2078,9 +2078,7 @@ function getProcessedLandsatScenes(studyArea,startYear,endYear,startJulian,endJu
     print('Apply Roy 2016 harmonization to OLI');
     var lsTMs = ls.filter(ee.Filter.equals('SATELLITE','LANDSAT_8').not());
     var lsOLIs = ls.filter(ee.Filter.equals('SATELLITE','LANDSAT_8'));
-    var temp = lsOLIs.map(harmonizationRoy);
-    print (temp, 'is temp');
-    lsOLIs = ee.ImageCollection(temp.addBands(lsOLIs.select('pixel_qa')));
+    lsOLIs = lsOLIs.map(harmonizationRoy);
     ls = ee.ImageCollection(lsTMs.merge(lsOLIs));
   }
   // Apply relevant cloud masking methods
