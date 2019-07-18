7b257a617cbed3be1326dd743768f38717c33545
leahs.campbell <leahs.campbell@gmail.com>
2019-07-18 16:15:43 +0000

    Updated changeDetectionLib.js (added 1 line, deleted 1 line).

--- changeDetectionLib.js
+++ changeDetectionLib.js
@@ -647,7 +647,7 @@ function prepTimeSeriesForLandTrendr(ts,indexName, run_params){
 
   run_params.timeSeries = tsT;
   var runMask = countMask.rename('insufficentDataMask');
-  var outputDict = {
+  var prepDict = {
     'run_params': run_params,
     'runMask':    runMask
   }
