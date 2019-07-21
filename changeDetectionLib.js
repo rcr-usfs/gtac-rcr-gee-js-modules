67d8fe2370f5967f04286031421e8a85a7139969
leahs.campbell <leahs.campbell@gmail.com>
2019-07-21 22:51:21 +0000

    Updated changeDetectionLib.js (added 2 lines, deleted 1 line).

--- changeDetectionLib.js
+++ changeDetectionLib.js
@@ -1001,7 +1001,8 @@ function applyLinearInterp(composites, nYearsInterpolate){
     composites = composites.select(['red','green','blue','nir','swir1','swir2']);
     
     // Find pixels/years with no data
-    var masks = composites.map(function(img){return img.mask().reduce(ee.Reducer.min()).byte().copyProperties(img, img.propertyNames())}).select([0],[ee.Date(img.get('system:time_start')).format('YYYY')]);
+    var masks = composites.map(function(img){return img.mask().reduce(ee.Reducer.min()).byte().copyProperties(img, img.propertyNames())}).select([0]);
+    masks = masks.map(function(img){return img.rename([ee.Date(img.get('system:time_start')).format('YYYY')])});
 
     //Perform linear interpolation        
     composites = linearInterp(composites, 365*nYearsInterpolate, -32768)
