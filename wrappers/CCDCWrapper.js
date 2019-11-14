ceb6d16a94bb5c1b1a233b83f41658e62a394733
iwhousman <iwhousman@gmail.com>
2019-11-14 19:00:35 +0000

    Updated wrappers/CCDCWrapper.js (added 2 lines, deleted 2 lines).

--- wrappers/CCDCWrapper.js
+++ wrappers/CCDCWrapper.js
@@ -167,8 +167,8 @@ Map.addLayer(processedScenes.select(['NDVI']),{},'ts',false);
 processedScenes = processedScenes.select(indexNames);
 var ccdc = ee.Algorithms.TemporalSegmentation.Ccdc(processedScenes, indexNames, ['green','swir2'], 6, 0.99, 1.33, 1,0);
 
-// var ccdcImage = ccdcLib.buildCcdcImage(ccdc,3);
-print(ccdc);
+var ccdcImage = ccdcLib.buildCcdcImage(ccdc,1);
+print(ccdcImage);
 // Map.addLayer(ccdcImage.select(['S1_tEnd']),{min:startYear,max:endYear},'CCDC end year')
 // print(ccdcImage)
 // Map.addLayer(ccdc,{},'cdc')
