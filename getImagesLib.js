2e1c0a30f504be1ad90ffa7ba835c2e83378a2c0
iwhousman <iwhousman@gmail.com>
2020-01-03 21:17:29 +0000

    Updated getImagesLib.js (added 2 lines, deleted 2 lines).

--- getImagesLib.js
+++ getImagesLib.js
@@ -2308,7 +2308,7 @@ function getSentinel2Wrapper(studyArea,startYear,endYear,startJulian,endJulian,
   if(correctScale === undefined || correctScale === null){correctScale = 250}
   if(exportComposites === undefined || exportComposites === null){exportComposites = false}
   if(outputName === undefined || outputName === null){outputName = 'Sentinel2-Composite'}
-  if(exportPathRoot === undefined || exportPathRoot === null){exportPathRoot = 'users/ianhousman/test'}
+  if(exportPathRoot === undefined || exportPathRoot === null){exportPathRoot = 'users/iwhousman/test'}
   if(crs === undefined || crs === null){crs = 'EPSG:5070'}
   if((scale === undefined || scale === null) && (transform === undefined || transform === null)){scale = 20;transform = null;}
   if(resampleMethod === undefined || resampleMethod === null){resampleMethod = 'near'}
