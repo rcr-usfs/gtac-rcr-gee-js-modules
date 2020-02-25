6e8244e36894db496805e34dc4fe833d97c758c7
iwhousman <iwhousman@gmail.com>
2020-02-25 18:15:33 +0000

    Updated getImagesLib.js (added 1 line, deleted 1 line).

--- getImagesLib.js
+++ getImagesLib.js
@@ -550,7 +550,7 @@ function getImageCollection(studyArea,startDate,endDate,startJulian,endJulian,
     //Join the TOA with SR QA bands
     print('Joining TOA with SR QA bands');
     print(ls.size(),lsTOAFMASK.size())
-    ls = joinCollections(ls.select([0,1,2,3,4,5,6]),lsTOAFMASK,false,'system:time_start');
+    ls = joinCollections(ls.select([0,1,2,3,4,5,6]),lsTOAFMASK,false,'system:index');
     print(ls)
   }
   
