44864b227b903766374e7b549f072b77ad1699f1
iwhousman <iwhousman@gmail.com>
2020-03-16 22:07:30 +0000

    Updated getImagesLib.js (added 1 line, deleted 1 line).

--- getImagesLib.js
+++ getImagesLib.js
@@ -342,7 +342,7 @@ function dailyMosaics(imgs){
   //Find the unique days
   var days = uniqueValues(imgs,'simpleTime');
   print(days)
-  print(imgs.aggregate_histogram('simpleTime'))
+  print(imgs.aggregate_histogram('simpleTime').keys())
   imgs = days.map(function(d){
     d = ee.Number.parse(d);
     d = ee.Date(d);
