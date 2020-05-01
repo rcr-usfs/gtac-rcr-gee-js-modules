e5524da4869e7e4b16ec4afc11f993d54eb0195c
iwhousman <iwhousman@gmail.com>
2020-05-01 20:09:54 +0000

    Updated wrappers/CCDCWrapper.js (added 2 lines, deleted 4 lines).

--- wrappers/CCDCWrapper.js
+++ wrappers/CCDCWrapper.js
@@ -16,10 +16,8 @@ var geometry =
 ///Module imports
 var getImagesLib = require('users/USFS_GTAC/modules:getImagesLib.js');
 var dLib = require('users/USFS_GTAC/modules:changeDetectionLib.js');
-// var ccdcLib = require('users/yang/CCDC:default');
 
-
-// dLib.getExistingChangeData();
+dLib.getExistingChangeData();
 ///////////////////////////////////////////////////////////////////////////////
 // Define user parameters:
 
