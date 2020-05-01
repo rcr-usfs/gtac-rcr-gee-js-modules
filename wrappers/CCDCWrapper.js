5c5de6ec4ffb5d7be286edcea5184938a721ad14
iwhousman <iwhousman@gmail.com>
2020-05-01 18:20:52 +0000

    Updated wrappers/CCDCWrapper.js (added 2 lines, deleted 2 lines).

--- wrappers/CCDCWrapper.js
+++ wrappers/CCDCWrapper.js
@@ -393,7 +393,7 @@ var ccdcImg = ee.ImageCollection('projects/CCDC/USA')
           .filterBounds(geometry)
           .mosaic();
 // // print(ccdcImg)
-var ccdcImgCoeffs = ccdcImg.select(['.*B2_coef_.*','.*B4_coef_.*'])//.divide(10000);
+var ccdcImgCoeffs = ccdcImg.select(['.*B2_coef_.*','.*B4_coef_.*']).divide(365.25);
 var ccdcImgT = ccdcImg.select(['.*tStart','.*tEnd']).divide(365.25);
 
 ccdcImg = ccdcImgCoeffs.addBands(ccdcImgT);
@@ -425,7 +425,7 @@ Map.addLayer(count,{min:1,max:nSegments},'Segment Count');
 var predictedSmall = predictCCDC(ccdcImgSmall,yearImages).select(['.*_predicted']);
 Map.addLayer(predictedSmall,{},'Predicted Small')
 Map.addLayer(ccdcImg)
-var predictedCONUS = predictCCDC(ccdcImg,yearImages).select(['.*_predicted']).map(function(img){return img.divide(100000).copyProperties(img,['system:time_start'])});
+var predictedCONUS = predictCCDC(ccdcImg,yearImages).select(['.*_predicted'])//.map(function(img){return img.divide(100000).copyProperties(img,['system:time_start'])});
 Map.addLayer(predictedCONUS,{},'Predicted CONUS')
   // print(ccdcImg);
 // Map.addLayer(ccdcImg,{},'ccdcImg',false);
