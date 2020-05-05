4fb39a24b32b2ef428f3c1f30057bbd7d5953df7
iwhousman <iwhousman@gmail.com>
2020-05-05 20:32:41 +0000

    Updated wrappers/CCDCWrapper.js (added 3 lines, deleted 3 lines).

--- wrappers/CCDCWrapper.js
+++ wrappers/CCDCWrapper.js
@@ -192,9 +192,9 @@ Map.addLayer(count,{min:1,max:nSegments},'Segment Count');
 processedScenes = processedScenes.map(getImagesLib.addYearYearFractionBand);
 ccdcParams.breakpointBands.push('.*_predicted');
 print(ccdcParams)
-// var changeYears = dLib.getCCDCChange(ccdcImg);
-// Map.addLayer(changeYears.lossYears.reduce(ee.Reducer.max()),{min:startYear,max:endYear,palette:dLib.lossYearPalette},'Most Recent Loss Year',false);
-// Map.addLayer(changeYears.gainYears.reduce(ee.Reducer.max()),{min:startYear,max:endYear,palette:dLib.gainYearPalette},'Most Recent Gain Year',false);
+var changeYears = dLib.getCCDCChange(ccdcImg);
+Map.addLayer(changeYears.lossYears.reduce(ee.Reducer.max()),{min:startYear,max:endYear,palette:dLib.lossYearPalette},'Most Recent Loss Year',false);
+Map.addLayer(changeYears.gainYears.reduce(ee.Reducer.max()),{min:startYear,max:endYear,palette:dLib.gainYearPalette},'Most Recent Gain Year',false);
 // Export.Image.toDrive(changeYears.lossYears.reduce(ee.Reducer.max()),)  
   
 //Predict CCDC model and visualize the actual vs. predicted
