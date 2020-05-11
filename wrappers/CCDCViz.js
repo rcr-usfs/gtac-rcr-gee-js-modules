c18d44b467e671e9e896d4afa8363be6e1868b52
iwhousman <iwhousman@gmail.com>
2020-05-11 17:34:04 +0000

    Updated wrappers/CCDCViz.js (added 4 lines, deleted 4 lines).

--- wrappers/CCDCViz.js
+++ wrappers/CCDCViz.js
@@ -17,8 +17,8 @@ var geometry =
 // var getImagesLib = require('users/USFS_GTAC/modules:getImagesLib.js');
 var dLib = require('users/USFS_GTAC/modules:changeDetectionLib.js');
 ///////////////////////////////////////////////////////////////////////
-var startYear = 2010;
-var endYear = 2020;
+var startYear = 1984;
+var endYear = 2021;
 var ccdcImg = ee.Image('users/iwhousman/test/CCDC_Collection/CCDC_Test3').reproject('EPSG:5070',null,30);
 Map.addLayer(ccdcImg,{},'CCDC Img',false);
 
@@ -74,7 +74,7 @@ var yearImages = ee.ImageCollection(ee.List.sequence(startYear,endYear+1,0.1).ma
 // Map.addLayer(count,{min:1,max:nSegments},'Segment Count');
 // // Map.addLayer(ccdcImgSmall.select(['.*tEnd']).selfMask().reduce(ee.Reducer.max()),{min:endYear-1,max:endYear},'Last Year');
   
-// var predicted = dLib.predictCCDC(ccdcImg,yearImages).select(['.*_predicted']);
+var predicted = dLib.predictCCDC(ccdcImg,yearImages).select(['.*_predicted']);
 // predicted = predicted.map(function(img){
 //   var nbr = img.normalizedDifference(['nir_predicted','red_predicted']).rename(['NBR_predicted_from_bands'])
 //   var ndvi = img.normalizedDifference(['nir_predicted','swir2_predicted']).rename(['NDVI_predicted_from_bands'])
@@ -82,7 +82,7 @@ var yearImages = ee.ImageCollection(ee.List.sequence(startYear,endYear+1,0.1).ma
   
   
 // })
-// Map.addLayer(predicted,{},'Predicted',false)
+Map.addLayer(predicted,{},'Predicted',false)
 
 
 // Map.addLayer(ccdcImg)
