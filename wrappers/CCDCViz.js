3a113ec3617d60bc0b4f0f1b06d13b81b6bc808e
iwhousman <iwhousman@gmail.com>
2020-05-13 22:20:34 +0000

    Updated wrappers/CCDCViz.js (added 1 line, deleted 1 line).

--- wrappers/CCDCViz.js
+++ wrappers/CCDCViz.js
@@ -17,7 +17,7 @@ var geometry =
 var getImagesLib = require('users/USFS_GTAC/modules:getImagesLib.js');
 var dLib = require('users/USFS_GTAC/modules:changeDetectionLib.js');
 ///////////////////////////////////////////////////////////////////////
-var startYear = 1984;
+var startYear = 2000;
 var endYear = 2021;
 var ccdcImg = ee.Image('users/iwhousman/test/CCDC_Collection/CCDC_Test6');//.reproject('EPSG:5070',null,30);
 Map.addLayer(ccdcImg,{},'CCDC Img',false);
