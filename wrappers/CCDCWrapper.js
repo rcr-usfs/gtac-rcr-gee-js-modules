ecc7ae763302a5f7f18b7b6a1270338a7b21571b
iwhousman <iwhousman@gmail.com>
2020-06-09 20:44:30 +0000

    Updated wrappers/CCDCWrapper.js (added 9 lines).

--- wrappers/CCDCWrapper.js
+++ wrappers/CCDCWrapper.js
@@ -206,6 +206,15 @@ ccdcParams.collection = processedScenes;
 //Run CCDC
 var ccdc = ee.Algorithms.TemporalSegmentation.Ccdc(ccdcParams);
 
+//Run EWMACD 
+var ewmacd = ee.Algorithms.TemporalSegmentation.Ewmacd({
+    timeSeries: processedScenes.select(['NDVI']), 
+    vegetationThreshold: -1, 
+    trainingStartYear: startYear, 
+    trainingEndYear: startYear+1, 
+    harmonicCount: 2
+  });
+Map.addLayer(ewmacd,{},'ewmacd',false)
 //Convert to image stack
 var ccdcImg = dLib.buildCcdcImage(ccdc, nSegments);
 // ccdcImg = ccdcImg.updateMask(ccdcImg.neq(-32768));
