771bd6269ea3d6d5cd41549185b5c46be04adc62
iwhousman <iwhousman@gmail.com>
2020-07-08 22:35:58 +0000

    Updated wrappers/getCombinedLandsatSentinel2Wrapper.js (added 53 lines, deleted 56 lines).

--- wrappers/getCombinedLandsatSentinel2Wrapper.js
+++ wrappers/getCombinedLandsatSentinel2Wrapper.js
@@ -6,33 +6,33 @@ var geometry = /* color: #d63000 */ee.Geometry.Polygon(
           [-114.40969985413568, 48.87781053836135]]]);
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
 //Module imports
-var getImagesLib = require('users/USFS_GTAC/modules:getImagesLib.js');
+var getImagesLib = require('users/USFS_GTAC/modules:getImagesLib2.js');
 ///////////////////////////////////////////////////////////////////////////////
 // Define user parameters:
-
+var args = {};
 // 1. Specify study area: Study area
 // Can specify a country, provide a fusion table  or asset table (must add 
 // .geometry() after it), or draw a polygon and make studyArea = drawnPolygon
-var studyArea = geometry;
+args.studyArea = geometry;
 
 // 2. Update the startJulian and endJulian variables to indicate your seasonal 
 // constraints. This supports wrapping for tropics and southern hemisphere.
 // startJulian: Starting Julian date 
 // endJulian: Ending Julian date
-var startJulian = 190;
-var endJulian = 250; 
+args.startJulian = 190;
+args.endJulian = 250; 
 
 // 3. Specify start and end years for all analyses
 // More than a 3 year span should be provided for time series methods to work 
 // well. If using Fmask as the cloud/cloud shadow masking method, this does not 
 // matter
-var startYear = 2014;
-var endYear = 2018;
+args.startYear = 2014;
+args.endYear = 2018;
 
 // 4. Specify an annual buffer to include imagery from the same season 
 // timeframe from the prior and following year. timeBuffer = 1 will result 
 // in a 3 year moving window
-var timebuffer = 0;
+args.timebuffer = 0;
 
 // 5. Specify the weights to be used for the moving window created by timeBuffer
 //For example- if timeBuffer is 1, that is a 3 year moving window
@@ -40,28 +40,28 @@ var timebuffer = 0;
 //In order to overweight the center year, you could specify the weights as
 //[1,5,1] which would duplicate the center year 5 times and increase its weight for
 //the compositing method
-var weights = [1];
+args.weights = [1];
 
 // 6. Choose medoid or median compositing method. 
 // Median tends to be smoother, while medoid retains 
 // single date of observation across all bands
 // If not exporting indices with composites to save space, medoid should be used
-var compositingMethod = 'medoid';
+args.compositingMethod = 'medoid';
 
 // 7. Choose Top of Atmospheric (TOA) or Surface Reflectance (SR) 
 // Specify TOA or SR
 //Sentinel 2 SR data currently have terrain correction applied
 //Best to use TOA if using S2 and Landsat together
-var toaOrSR = 'TOA';
+args.toaOrSR = 'TOA';
 
 //Whether to convert S2 images from the military grid reference system(MGRS) tiles to daily mosaics to avoid arbitrary
 //MGRS tile artifacts or not. In most cases, it is best to set this to true.
-var convertToDailyMosaics = true;
+args.convertToDailyMosaics = true;
 
 
 // 8. Choose whether to include Landat 7
 // Generally only included when data are limited
-var includeSLCOffL7 = true;
+args.includeSLCOffL7 = true;
 
 //9. Whether to defringe L5
 //Landsat 5 data has fringes on the edges that can introduce anomalies into 
@@ -83,66 +83,66 @@ var defringeL5 = false;
 
 //CloudScore and TDOM switches- for both Sentinel 2 and Landsat
 //We generally use these
-var applyCloudScore = true;
-var applyTDOM = true;
+args.applyCloudScore = true;
+args.applyTDOM = true;
 
 //S2 only cloud/cloud shadow masking methods switches- generally do not use these
 //QA band method is fast but is generally awful- don't use if you like good composites
 //Shadow shift is intended if you don't have a time series to use for TDOM or just want individual images
 //It will commit any dark area that the cloud mask is cast over (water, hill shadows, etc)
-var applyQABand = false;
-var applyShadowShift = false;
+args.applyQABand = false;
+args.applyShadowShift = false;
 //Height of clouds to use to project cloud shadows
-var cloudHeights = ee.List.sequence(500,10000,500);
+args.cloudHeights = ee.List.sequence(500,10000,500);
 
 //Fmask switches- only for Landsat
 //Generally we do use these
-var applyFmaskCloudMask = true;
-var applyFmaskCloudShadowMask = true;
-var applyFmaskSnowMask = false;
+args.applyFmaskCloudMask = true;
+args.applyFmaskCloudShadowMask = true;
+args.applyFmaskSnowMask = false;
 
 // 11. Cloud and cloud shadow masking parameters.
-// If cloudScoreTDOM is chosen
-// cloudScoreThresh: If using the cloudScoreTDOMShift method-Threshold for cloud 
+// If cloudScore  is chosen
+// cloudScoreThresh: If using the cloudScore or cloudProbability method-Threshold for cloud 
 //    masking (lower number masks more clouds.  Between 10 and 30 generally 
 //    works best)
-var cloudScoreThresh = 20;
+args.cloudScoreThresh = 20;
 
 //Whether to find if an area typically has a high cloudScore
 //If an area is always cloudy, this will result in cloud masking omission
 //For bright areas that may always have a high cloudScore
 //but not actually be cloudy, this will result in a reduction of commission errors
 //This procedure needs at least 5 years of data to work well
-var performCloudScoreOffset = true;
+args.performCloudScoreOffset = true;
 
 // If performCloudScoreOffset = true:
 //Percentile of cloud score to pull from time series to represent a minimum for 
 // the cloud score over time for a given pixel. Reduces comission errors over 
 // cool bright surfaces. Generally between 5 and 10 works well. 0 generally is a
 // bit noisy but may be necessary in persistently cloudy areas
-var cloudScorePctl = 10;
+args.cloudScorePctl = 10;
 
 // zScoreThresh: Threshold for cloud shadow masking- lower number masks out 
 //    less. Between -0.8 and -1.2 generally works well
-var zScoreThresh = -1;
+args.zScoreThresh = -1;
 
 // shadowSumThresh: Sum of IR bands to include as shadows within TDOM and the 
 //    shadow shift method (lower number masks out less)
-var shadowSumThresh = 0.35;
+args.shadowSumThresh = 0.35;
 
 // contractPixels: The radius of the number of pixels to contract (negative 
 //    buffer) clouds and cloud shadows by. Intended to eliminate smaller cloud 
 //    patches that are likely errors
 // (1.5 results in a -1 pixel buffer)(0.5 results in a -0 pixel buffer)
 // (1.5 or 2.5 generally is sufficient)
-var contractPixels = 1.5; 
+args.contractPixels = 1.5; 
 
 // dilatePixels: The radius of the number of pixels to dilate (buffer) clouds 
 //    and cloud shadows by. Intended to include edges of clouds/cloud shadows 
 //    that are often missed
 // (1.5 results in a 1 pixel buffer)(0.5 results in a 0 pixel buffer)
 // (2.5 or 3.5 generally is sufficient)
-var dilatePixels = 2.5;
+args.dilatePixels = 2.5;
 
 //Choose the resampling method: 'aggregate','near', 'bilinear', or 'bicubic'
 //Defaults to 'aggregate' for Sentinel 2 and 'near' for Landsat
@@ -153,14 +153,14 @@ var dilatePixels = 2.5;
 //If method other than 'near' is chosen, any map drawn on the fly that is not
 //reprojected, will appear blurred or not really represented properly
 //Use .reproject to view the actual resulting image (this will slow it down)
-var landsatResampleMethod = 'near';
+args.landsatResampleMethod = 'near';
 
-var sentinel2ResampleMethod = 'aggregate';
+args.sentinel2ResampleMethod = 'aggregate';
 
 //Choose whether to use the Chastain et al 2019(https://www.sciencedirect.com/science/article/pii/S0034425718305212)
 //harmonization method
 //All harmonization models apply a rather small correction and are likely not needed
-var runChastainHarmonization = true;
+args.runChastainHarmonization = true;
 
 //If available, bring in preComputed cloudScore offsets and TDOM stats
 //Set to null if computing on-the-fly is wanted
@@ -168,17 +168,17 @@ var runChastainHarmonization = true;
 //and are appropriate to use for any time period within the growing season
 //The cloudScore offset is generally some lower percentile of cloudScores on a pixel-wise basis
 var preComputedCloudScoreOffset = ee.ImageCollection('projects/USFS/TCC/cloudScore_stats').mosaic();
-var preComputedLandsatCloudScoreOffset = preComputedCloudScoreOffset.select(['Landsat_CloudScore_p'+cloudScorePctl.toString()]);
-var preComputedSentinel2CloudScoreOffset = preComputedCloudScoreOffset.select(['Sentinel2_CloudScore_p'+cloudScorePctl.toString()]);
+args.preComputedLandsatCloudScoreOffset = preComputedCloudScoreOffset.select(['Landsat_CloudScore_p'+cloudScorePctl.toString()]);
+args.preComputedSentinel2CloudScoreOffset = preComputedCloudScoreOffset.select(['Sentinel2_CloudScore_p'+cloudScorePctl.toString()]);
 
 //The TDOM stats are the mean and standard deviations of the two bands used in TDOM
 //By default, TDOM uses the nir and swir1 bands
 var preComputedTDOMStats = ee.ImageCollection('projects/USFS/TCC/TDOM_stats').mosaic().divide(10000);
-var preComputedLandsatTDOMMeans = preComputedTDOMStats.select(['Landsat_nir_mean','Landsat_swir1_mean']);
-var preComputedLandsatTDOMStdDevs = preComputedTDOMStats.select(['Landsat_nir_stdDev','Landsat_swir1_stdDev']);
+args.preComputedLandsatTDOMIRMean = preComputedTDOMStats.select(['Landsat_nir_mean','Landsat_swir1_mean']);
+args.preComputedLandsatTDOMIRStdDev = preComputedTDOMStats.select(['Landsat_nir_stdDev','Landsat_swir1_stdDev']);
 
-var preComputedSentinel2TDOMMeans = preComputedTDOMStats.select(['Sentinel2_nir_mean','Sentinel2_swir1_mean']);
-var preComputedSentinel2TDOMStdDevs = preComputedTDOMStats.select(['Sentinel2_nir_stdDev','Sentinel2_swir1_stdDev']);
+args.preComputedSentinel2TDOMMeans = preComputedTDOMStats.select(['Sentinel2_nir_mean','Sentinel2_swir1_mean']);
+args.preComputedSentinel2TDOMStdDevs = preComputedTDOMStats.select(['Sentinel2_nir_stdDev','Sentinel2_swir1_stdDev']);
 
 // 12. correctIllumination: Choose if you want to correct the illumination using
 // Sun-Canopy-Sensor+C correction. Additionally, choose the scale at which the
@@ -217,24 +217,21 @@ var scale = null;
 ///////////////////////////////////////////////////////////////////////
 //Start function calls
 ////////////////////////////////////////////////////////////////////////////////
-function test(args,second){
-  print(typeof(args))
-}
-test(args,second)
+
 ///////////////////////////////////////////////////////////////
-// var processedAndComposites = getImagesLib.getLandsatAndSentinel2HybridWrapper(studyArea,startYear,endYear,startJulian,endJulian,
-//   timebuffer,weights,compositingMethod,
-//   toaOrSR,includeSLCOffL7,defringeL5,
-//   applyQABand,applyShadowShift,
-//   applyCloudScore,applyTDOM,
-//   applyFmaskCloudMask,applyFmaskCloudShadowMask,applyFmaskSnowMask,
-//   cloudHeights,cloudScoreThresh,performCloudScoreOffset,cloudScorePctl,
-//   zScoreThresh,shadowSumThresh,
-//   contractPixels,dilatePixels,landsatResampleMethod,sentinel2ResampleMethod,convertToDailyMosaics,runChastainHarmonization,
-//   correctIllumination,correctScale,
-//   exportComposites,outputName,exportPathRoot,crs,transform,scale,
-//   preComputedLandsatCloudScoreOffset,preComputedLandsatTDOMMeans,preComputedLandsatTDOMStdDevs,
-//   preComputedSentinel2CloudScoreOffset,preComputedSentinel2TDOMMeans,preComputedSentinel2TDOMStdDevs);
+var processedAndComposites = getImagesLib.getLandsatAndSentinel2HybridWrapper(studyArea,startYear,endYear,startJulian,endJulian,
+  timebuffer,weights,compositingMethod,
+  toaOrSR,includeSLCOffL7,defringeL5,
+  applyQABand,applyShadowShift,
+  applyCloudScore,applyTDOM,
+  applyFmaskCloudMask,applyFmaskCloudShadowMask,applyFmaskSnowMask,
+  cloudHeights,cloudScoreThresh,performCloudScoreOffset,cloudScorePctl,
+  zScoreThresh,shadowSumThresh,
+  contractPixels,dilatePixels,landsatResampleMethod,sentinel2ResampleMethod,convertToDailyMosaics,runChastainHarmonization,
+  correctIllumination,correctScale,
+  exportComposites,outputName,exportPathRoot,crs,transform,scale,
+  preComputedLandsatCloudScoreOffset,preComputedLandsatTDOMMeans,preComputedLandsatTDOMStdDevs,
+  preComputedSentinel2CloudScoreOffset,preComputedSentinel2TDOMMeans,preComputedSentinel2TDOMStdDevs);
 
 // //Separate into scenes and composites for subsequent analysis
 // var processedScenes = processedAndComposites[0];
