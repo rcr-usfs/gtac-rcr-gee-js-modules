5e36c7dccd60cd850c32861ab801f5f6befa1092
iwhousman <iwhousman@gmail.com>
2020-07-09 19:01:18 +0000

    Updated getImagesLib2.js (added 5 lines, deleted 10 lines).

--- getImagesLib2.js
+++ getImagesLib2.js
@@ -75,7 +75,6 @@ function prepArgumentsObject(args,defaultArgs){
       
   if(typeof(argList[0]) === 'object' && argList.length === 1 && !firstArgumentIsEEObj){
     argsAreObject = true;
-    // outArgs = copyObj(argList[0]);
   }
   //Iterate through each expected argument to create the obj with all parameters
   Object.keys(defaultArgs).forEach(function(key, i) {
