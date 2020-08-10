842d349532d7c2a5601957c8cef0144157874554
iwhousman <iwhousman@gmail.com>
2020-08-10 15:08:40 +0000

    Updated wrappers/Basic-Two-Date-Change-Detection.js (added 31 lines, deleted 63 lines).

--- wrappers/Basic-Two-Date-Change-Detection.js
+++ wrappers/Basic-Two-Date-Change-Detection.js
@@ -14,41 +14,23 @@ var geometry =
           [-129.94603840348452, 56.98337037289282]]], null, false);
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
 var getImagesLib = require('users/USFS_GTAC/modules:getImagesLib2.js');
-//If available, bring in preComputed cloudScore offsets and TDOM stats
-//Set to null if computing on-the-fly is wanted
-//These have been pre-computed for all CONUS for Landsat and Setinel 2 (separately)
-//and are appropriate to use for any time period within the growing season
-//The cloudScore offset is generally some lower percentile of cloudScores on a pixel-wise basis
-var preComputedCloudScoreOffset = ee.ImageCollection('projects/USFS/TCC/cloudScore_stats').mosaic();
-var preComputedLandsatCloudScoreOffset = preComputedCloudScoreOffset.select(['Landsat_CloudScore_p10']);
-var preComputedSentinel2CloudScoreOffset = preComputedCloudScoreOffset.select(['Sentinel2_CloudScore_p10']);
-
-//The TDOM stats are the mean and standard deviations of the two bands used in TDOM
-//By default, TDOM uses the nir and swir1 bands
-var preComputedTDOMStats = ee.ImageCollection('projects/USFS/TCC/TDOM_stats').mosaic().divide(10000);
-var preComputedLandsatTDOMIRMean = preComputedTDOMStats.select(['Landsat_nir_mean','Landsat_swir1_mean']);
-var preComputedLandsatTDOMIRStdDev = preComputedTDOMStats.select(['Landsat_nir_stdDev','Landsat_swir1_stdDev']);
-
-var preComputedSentinel2TDOMIRMean = preComputedTDOMStats.select(['Sentinel2_nir_mean','Sentinel2_swir1_mean']);
-var preComputedSentinel2TDOMIRStdDev = preComputedTDOMStats.select(['Sentinel2_nir_stdDev','Sentinel2_swir1_stdDev']);
-//////////////////////////////////////////////////////////////
-
 var hiFormViz = {min: 0.05, max: 0.15, gamma: 1.2,bands:'red,green,blue'};
-
+//////////////////////////////////////////////////////////////
 //User parameters
 
 //Choose dates for pre and post
-var preStartYear = 2020;
-var preEndYear = 2020;
+var preStartYear = 2019;
+var preEndYear = 2019;
 
 var postStartYear = 2020;
 var postEndYear = 2020;
 
-var preStartJulian = ee.Date.fromYMD(9999,5,1).getRelative('day','year').add(1).getInfo();
-var preEndJulian = ee.Date.fromYMD(9999,7,1).getRelative('day','year').add(1).getInfo();
+//Offset by 1 day if you're looking at a leap year
+var preStartJulian = ee.Date.fromYMD(5,7,15).getRelative('day','year').add(1).getInfo();
+var preEndJulian = ee.Date.fromYMD(5,8,15).getRelative('day','year').add(1).getInfo();
 
-var postStartJulian = ee.Date.fromYMD(9999,7,28).getRelative('day','year').add(1).getInfo();
-var postEndJulian = ee.Date.fromYMD(9999,7,30).getRelative('day','year').add(1).getInfo();
+var postStartJulian = ee.Date.fromYMD(5,7,29).getRelative('day','year').add(1).getInfo();
+var postEndJulian = ee.Date.fromYMD(5,7,31).getRelative('day','year').add(1).getInfo();
 
 var studyArea = geometry;
 
@@ -68,47 +50,32 @@ var lossThresh = [-0.2,-0.2,-0.2];
 //If you want to find loss in the majority of bands, use ee.Reducer.mode() for lossReducer
 var lossReducer = ee.Reducer.mode();
 
-var treeMask = ee.Image("USGS/NLCD/NLCD2016").select(['percent_tree_cover']).gte(10).selfMask();
-
+var treeMask = ee.ImageCollection("USGS/NLCD")
+                .filter(ee.Filter.calendarRange(2011,2011,'year'))
+                .select(['percent_tree_cover']).mosaic().gte(10).selfMask();
+Map.addLayer(treeMask,{min:1,max:1,palette:'080'},'Tree Mask',false);
 //End User Inputs
 ////////////////////////////////////////////////////////////////////////////
 //Get images
-var preComposite = getImagesLib.getProcessedSentinel2Scenes({
+var images = getImagesLib.getProcessedSentinel2Scenes({
   studyArea:studyArea,
-  startYear:preStartYear,
-  endYear:preEndYear,
-  startJulian:preStartJulian,
-  endJulian:preEndJulian,
-  toaOrSR :'SR',
-  'applyCloudProbability':false,
-  'applyCloudScore':true,
-  // preComputedLandsatCloudScoreOffset:preComputedLandsatCloudScoreOffset,
-  // preComputedLandsatTDOMIRMean:preComputedLandsatTDOMIRMean,
-  // preComputedLandsatTDOMIRStdDev:preComputedLandsatTDOMIRStdDev,
-  preComputedCloudScoreOffset:preComputedSentinel2CloudScoreOffset,
-  preComputedTDOMIRMean:preComputedSentinel2TDOMIRMean,
-  preComputedTDOMIRStdDev:preComputedSentinel2TDOMIRStdDev,
-  // includeSLCOffL7:true
+  startYear:preStartYear-1,
+  endYear:postEndYear+1,
+  startJulian:Math.min(preStartJulian,postStartJulian),
+  endJulian:Math.max(preEndJulian,postEndJulian),
+  toaOrSR :'TOA',
+  performCloudScoreOffset:false,
+  applyCloudProbability:false,
+  applyCloudScore:true,
+  applyTDOM:false,
+  convertToDailyMosaics:false,
   });
+var preComposite = images.filter(ee.Filter.calendarRange(preStartYear,preEndYear,'year'))
+                  .filter(ee.Filter.calendarRange(preStartJulian,preEndJulian));
 
-var postComposite  = getImagesLib.getProcessedSentinel2Scenes({
-  studyArea:studyArea,
-  startYear:postStartYear,
-  endYear:postEndYear,
-  startJulian:postStartJulian,
-  endJulian:postEndJulian,
-  toaOrSR :'SR',
-  'applyCloudProbability':false,
-  'applyCloudScore':true,
-  // preComputedLandsatCloudScoreOffset:preComputedLandsatCloudScoreOffset,
-  // preComputedLandsatTDOMIRMean:preComputedLandsatTDOMIRMean,
-  // preComputedLandsatTDOMIRStdDev:preComputedLandsatTDOMIRStdDev,
-  preComputedCloudScoreOffset:preComputedSentinel2CloudScoreOffset,
-  preComputedTDOMIRMean:preComputedSentinel2TDOMIRMean,
-  preComputedTDOMIRStdDev:preComputedSentinel2TDOMIRStdDev,
-  // includeSLCOffL7:true
-  
-  });
+
+var postComposite  = images.filter(ee.Filter.calendarRange(postStartYear,postEndYear,'year'))
+                  .filter(ee.Filter.calendarRange(postStartJulian,postEndJulian));
 
 //Get band names since reducers will sometimes rename bands
 var bns = ee.Image(preComposite.first()).bandNames();
@@ -133,3 +100,4 @@ Map.addLayer(loss,{min:1,max:1,palette:'800'},'Loss',true);
 
 
 Map.setOptions('HYBRID');
+// Map.setCenter(-132.396, 56.4711, 7);
