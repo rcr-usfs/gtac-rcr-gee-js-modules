2043bc197ba774cb925bc3d01fea330052e4f6be
ian.housman <ian.housman@gmail.com>
2020-09-02 22:06:45 +0000

    Updated wrappers/CCDCViz.js (added 16 lines, deleted 24 lines).

--- wrappers/CCDCViz.js
+++ wrappers/CCDCViz.js
@@ -1,29 +1,21 @@
-/**** Start of imports. If edited, may not auto-convert in the playground. ****/
-var geometry = 
-    /* color: #d63000 */
-    /* shown: false */
-    /* displayProperties: [
-      {
-        "type": "rectangle"
-      }
-    ] */
-    ee.Geometry.Polygon(
-        [[[-108.28163961476221, 39.25451132142729],
-          [-108.28163961476221, 36.68265794564914],
-          [-104.55727438038721, 36.68265794564914],
-          [-104.55727438038721, 39.25451132142729]]], null, false);
-/***** End of imports. If edited, may not auto-convert in the playground. *****/
 ///Module imports
-// var getImagesLib = require('users/USFS_GTAC/modules:getImagesLib2.js');
 var dLib = require('users/USFS_GTAC/modules:changeDetectionLib.js');
-
 ///////////////////////////////////////////////////////////////////////
-
+//Bring in ccdc image asset
+//This is assumed to be an image of arrays that is returned from the ee.Algorithms.TemporalSegmentation.Ccdc method
 var ccdcImg = ee.Image('users/iwhousman/test/ChangeCollection/CCDC-Test3');
 
+//Specify which harmonics to use when predicting the CCDC model
+//CCDC exports the first 3 harmonics (1 cycle/yr, 2 cycles/yr, and 3 cycles/yr)
+//If you only want to see yearly patterns, specify [1]
+//If you would like a tighter fit in the predicted value, include the second or third harmonic as well [1,2,3]
 var whichHarmonics = [1,2,3];
-var changeDetectionBandName = 'NDVI';
 
+//Specify which band to use for loss and gain. 
+//This is most important for the loss and gain magnitude since the year of change will be the same for all years
+var changeDetectionBandName = 'NDVI';
+//////////////////////////////////////////////////////////////////////
+//Pull out some info about the ccdc image
 var startJulian = ccdcImg.get('startJulian').getInfo();
 var endJulian = ccdcImg.get('endJulian').getInfo();
 var startYear = ccdcImg.get('startYear').getInfo();
