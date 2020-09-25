4cd4ddde7f691b8745c6d4448ce45414439d4ac5
leahs.campbell <leahs.campbell@gmail.com>
2020-09-25 22:28:19 +0000

    Updated changeDetectionLib.js (added 1 line, deleted 1 line).

--- changeDetectionLib.js
+++ changeDetectionLib.js
@@ -1813,7 +1813,7 @@ function getCCDCSegCoeffs(timeImg,ccdcImg,fillGaps){
 //Function to get yearly ccdc coefficients. 
 //To have any break be contained within the calendar year that it occurred, set yearEndMonth = 12, yearEndDay = 31
 function annualizeCCDC(ccdcImg, startYear, endYear, startJulian, endJulian, yearEndMonth, yearEndDay){
-  var timeImgs = getTimeImageCollection(startYear-1,endYear,startJulian,endJulian,1,yearEndMonth, yearEndDay);
+  var timeImgs = getTimeImageCollection(startYear,endYear,startJulian,endJulian,1,yearEndMonth, yearEndDay);
   var timeBandName = ee.Image(timeImgs.first()).select([0]).bandNames().get(0);
   timeImgs = timeImgs.map(function(img){return getCCDCSegCoeffs(img,ccdcImg,true)});
   return timeImgs;
