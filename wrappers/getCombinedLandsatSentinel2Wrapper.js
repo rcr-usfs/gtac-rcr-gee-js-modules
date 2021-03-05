92539e8fbb54cc57f077539069e54ccbd70cce52
ian.housman <ian.housman@usda.gov>
2021-03-05 19:42:28 +0000

    Updated wrappers/getCombinedLandsatSentinel2Wrapper.js (added 2 lines, deleted 1 line).

--- wrappers/getCombinedLandsatSentinel2Wrapper.js
+++ wrappers/getCombinedLandsatSentinel2Wrapper.js
@@ -96,7 +96,8 @@ args.cloudHeights = ee.List.sequence(500,10000,500);
 //This method works really well
 args.applyCloudProbability = true;
 
-//If cloudProbability is chosen, choose a threshold (generally somewhere around 40-60 works well)
+//If cloudProbability is chosen, choose a threshold 
+//(generally somewhere around 40-60 works well)
 args.cloudProbThresh = 40;
 
 //Fmask switches- only for Landsat
