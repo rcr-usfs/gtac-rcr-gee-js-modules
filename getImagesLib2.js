23fd11d865fad4fe6ac5eb1d49caf2e3713dd4b8
ian.housman <ian.housman@usda.gov>
2021-03-10 20:38:33 +0000

    Updated getImagesLib2.js (added 2 lines, deleted 2 lines).

--- getImagesLib2.js
+++ getImagesLib2.js
@@ -3492,7 +3492,7 @@ var HoCalcGreenness = function (img) {
 };
 
 // Apply bloom detection algorithm
-var HoCalcAlgorithm1 = function(image) {
+exports.HoCalcAlgorithm1 = function(image) {
   var truecolor = 1;  // show true color image as well
   var testThresh = false; // add a binary image classifying into "bloom"and "non-bloom
   var bloomThreshold = 0.02346; //threshold for classification fit based on other data
@@ -3519,7 +3519,7 @@ var HoCalcAlgorithm1 = function(image) {
 };
 //
 // Apply bloom detection algorithm
-var HoCalcAlgorithm2 = function(image) {
+exports.HoCalcAlgorithm2 = function(image) {
   // Algorithm 2 based on: 
   // Matthews, M. (2011) A current review of empirical procedures 
   //  of remote sensing in inland and near-coastal transitional 
