a878a3e6708a135b64132256db593acaf6e3221c
ian.housman <ian.housman@gmail.com>
2022-03-24 20:17:57 +0000

    Updated getImagesLib.js (added 1 line, deleted 1 line).

--- getImagesLib.js
+++ getImagesLib.js
@@ -376,7 +376,7 @@ function getS2(studyArea,startDate,endDate,startJulian,endJulian,resampleMethod,
   if(convertToDailyMosaics === undefined || convertToDailyMosaics === null){convertToDailyMosaics = true}
   if(toaOrSR === undefined || toaOrSR === null){toaOrSR = 'TOA'}
   toaOrSR = toaOrSR.toUpperCase();
-  var s2CollectionDict = {'TOA':'COPERNICUS/S2','SR':'COPERNICUS/S2_SR'};
+  var s2CollectionDict = {'TOA':'COPERNICUS/S2_HARMONIZED','SR':'COPERNICUS/S2_SR_HARMONIZED'};
   var sensorBandDict = {
       'SR': ['B1','B2','B3','B4','B5','B6','B7','B8','B8A', 'B9', 'B11','B12'],
       'TOA': ['B1','B2','B3','B4','B5','B6','B7','B8','B8A', 'B9', 'B10', 'B11','B12']
