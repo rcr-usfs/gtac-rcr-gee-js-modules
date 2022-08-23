05ef8921c26d1f6fea69c52b65cf190993cdbe4a
ian.housman <ian.housman@usda.gov>
2022-08-23 16:06:52 +0000

    Updated getImagesLib.js (added 2 lines, deleted 2 lines).

--- getImagesLib.js
+++ getImagesLib.js
@@ -505,7 +505,7 @@ function dailyMosaics(imgs){
   imgs = dayOrbits.map(function(d){
     var date = ee.Date(ee.String(d).split('_').get(0));
     var orbit = ee.Number.parse(ee.String(d).split('_').get(1));
-    var t = imgs.filterDate(date,date.advance(2,'day'))
+    var t = imgs.filterDate(date,date.advance(1,'day'))
             .filter(ee.Filter.eq('SENSING_ORBIT_NUMBER',orbit));
     var f = ee.Image(t.first());
     t = t.mosaic();
