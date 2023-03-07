4edc1905be032251fc0760c20c9b46d3551d46f2
ian.housman <ian.housman@usda.gov>
2023-03-07 20:38:46 +0000

    Updated getImagesLib.js (added 27 lines, deleted 1 line).

--- getImagesLib.js
+++ getImagesLib.js
@@ -2864,6 +2864,16 @@ function getProcessedSentinel2Scenes(){
           .map(getTasseledCap)
           .map(simpleAddTCAngles);  
   
+  
+  if(['bilinear','bicubic'].indexOf(args.resampleMethod) > -1){
+    print('Setting resample method to ',args.resampleMethod);
+    s2s = s2s.map(function(img){return img.resample(args.resampleMethod)});
+  }
+  else if(args.resampleMethod === 'aggregate'){
+    print('Setting to aggregate instead of resample ');
+    s2s = s2s.map(function(img){return img.reduceResolution(ee.Reducer.mean(), true, 64)});
+  }
+  
   //Add sensor band
   s2s = s2s.map(function(img){return addSensorBand(img,'sentinel2',args.toaOrSR)});
   
