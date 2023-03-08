95d73fd0fc22821a2fd31b560d227758e13da1a0
ian.housman <ian.housman@usda.gov>
2023-03-08 17:14:27 +0000

    Updated getImagesLib.js (added 5 lines, deleted 15 lines).

--- getImagesLib.js
+++ getImagesLib.js
@@ -624,6 +624,9 @@ var landsat_C2_L2_rescale_dict = {
   'C1':{'refl_mult':0.0001,'refl_add':0,'temp_mult':0.1,'temp_add':0},
   'C2':{'refl_mult':0.0000275,'refl_add':-0.2,'temp_mult':0.00341802,'temp_add':149.0},
   };
+
+// Specify Landsat continuous bands if resampling is set to something other than near
+var landsat_continuous_bands = ['blue','green','red','nir','swir1','temp', 'swir2'];
 // Set up bands and corresponding band names
 var landsatSensorBandDict = {
   'C1_L4_TOA':['B1','B2','B3','B4','B5','B6','B7','BQA'],
