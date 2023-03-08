7ddbdb27f1ad7c8e923c4ee79da22e90e4d16c2c
ian.housman <ian.housman@usda.gov>
2023-03-08 17:31:08 +0000

    Updated getImagesLib.js (added 2 lines, deleted 1 line).

--- getImagesLib.js
+++ getImagesLib.js
@@ -3898,6 +3898,7 @@ exports.collectionToImage = collectionToImage;
 exports.getImageCollection = getImageCollection;
 exports.getLandsat = getLandsat;
 exports.getS2 = getS2;
+exports.getSentinel2 = getSentinel2;
 exports.vizParamsFalse = vizParamsFalse;
 exports.vizParamsTrue = vizParamsTrue;
 exports.vizParamsFalse10k = vizParamsFalse10k;
