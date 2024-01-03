9aa60c5635ee982b8c393e124babd91847a33ed1
ian.housman <ian.housman@usda.gov>
2024-01-03 20:47:13 +0000

    Updated changeDetectionLib.js (added 86 lines, deleted 4 lines).

--- changeDetectionLib.js
+++ changeDetectionLib.js
@@ -1083,7 +1083,7 @@ function simpleLTFit(ltStack,startYear,endYear,indexName,arrayMode,maxSegs){
 }
 
 // Wrapper function to iterate across multiple LT band/index values
-function batchSimpleLTFit(ltStacks,startYear,endYear,indexNames,bandPropertyName,arrayMode,maxSegs){
+function batchSimpleLTFit(ltStacks,startYear,endYear,indexNames,bandPropertyName,arrayMode,maxSegs,multBy){
   if(bandPropertyName === null || bandPropertyName === undefined){
     bandPropertyName = 'band';
   }
@@ -1091,16 +1091,16 @@ function batchSimpleLTFit(ltStacks,startYear,endYear,indexNames,bandPropertyName
   if(indexNames === null || indexNames === undefined){
     indexNames = ltStacks.aggregate_histogram(bandPropertyName).keys().getInfo();
   }
-
+  multBy = multBy || 1;
   // Iterate across each band/index and get the fitted, mag, slope, etc
   var lt_fit;
   indexNames.map(function(bn){
     var ltt = ltStacks.filter(ee.Filter.eq('band',bn)).max();
 
     if(lt_fit === undefined){
-      lt_fit = simpleLTFit(ltt,startYear,endYear, bn,arrayMode,maxSegs);
+      lt_fit = simpleLTFit(ltt,startYear,endYear, bn,arrayMode,maxSegs,multBy);
     }else{
-      lt_fit = getImagesLib.joinCollections(lt_fit, simpleLTFit(ltt,startYear,endYear,bn,arrayMode,maxSegs), false);
+      lt_fit = getImagesLib.joinCollections(lt_fit, simpleLTFit(ltt,startYear,endYear,bn,arrayMode,maxSegs,multBy), false);
     }
   });
   return lt_fit;
@@ -2391,3 +2391,85 @@ exports.getTimeImageCollection = getTimeImageCollection;
 exports.getTimeImageCollectionFromComposites = getTimeImageCollectionFromComposites;
 exports.ccdcChangeDetection = ccdcChangeDetection;
 
+
+//####################################################################################################
+// Define user parameters:
+// ####################################################################################################
+// Define user parameters:
+
+// Specify which years to look at 
+// Available years are 1984-present
+var startYear = 1984;
+var endYear = 2023;
+
+// Which property stores which band/index LandTrendr was run across
+var bandPropertyName = 'band';
+
+// Specify which bands to run across
+// Set to None to run all available bands
+// Available bands include: ['NBR', 'NDMI', 'NDSI', 'NDVI', 'blue', 'brightness', 'green', 'greenness', 'nir', 'red', 'swir1', 'swir2', 'tcAngleBG', 'wetness']
+var bandNames =null;
+
+// Specify if output is an array image or not
+var arrayMode = true;
+// ####################################################################################################
+// Bring in LCMS LandTrendr outputs (see other examples that include LCMS final data)
+var lt = ee.ImageCollection('projects/lcms-tcc-shared/assets/CONUS/Base-Learners/LandTrendr-Collection');
+print('Available bands/indices: ',lt.aggregate_histogram(bandPropertyName).keys().getInfo());
+
+var lt_props = lt.first().toDictionary().getInfo();
+print(lt_props);
+
+// Convert stacked outputs into collection of fitted, magnitude, slope, duration, etc values for each year
+// Divide by 10000 (0.0001) so values are back to original values (0-1 or -1-1)
+var lt_fit = batchSimpleLTFit(lt,startYear,endYear,bandNames,bandPropertyName,arrayMode,lt_props['maxSegments'],0.0001);
+print(lt_fit.first().getInfo())
+// Vizualize image collection for charting (opacity set to 0 so it will chart but not be visible)
+Map.addLayer(lt_fit.select(['NBR_LT_fitted']),{'opacity':0},'LT Fit TS');
+
+// Visualize single year fitted landTrendr composite
+// Set to only run if no bandNames are specified
+if(bandNames === null || bandNames === undefined){
+  // Get fitted bandnames
+  var fitted_bns = lt_fit.select(['.*_fitted']).first().bandNames();
+  var out_bns = fitted_bns.map(function(bn){return ee.String(bn).split('_').get(0)});
+
+  // Filter out next to last year
+  var lt_synth = lt_fit.select(fitted_bns,out_bns)
+            .filter(ee.Filter.calendarRange(endYear-1,endYear-1,'year')).first();
+
+  // Visualize as you would a composite
+  Map.addLayer(lt_synth,vizParamsFalse,'Synthetic Composite');
+
+}
+// Iterate across each band to look for areas of change
+if(bandNames === null || bandNames === undefined){bandNames=['NBR']}
+bandNames.map(function(bandName){
+  // Do basic change detection with raw LT output
+  var ltt = lt.filter(ee.Filter.eq(bandPropertyName,bandName)).mosaic();
+  ltt = multLT(ltt,changeDirDict[bandName]*0.0001);
+ 
+  var lossMagThresh = -0.15;
+  var lossSlopeThresh = -0.1;
+  var gainMagThresh = 0.1;
+  var gainSlopeThresh = 0.1;
+  var slowLossDurationThresh = 3;
+  var chooseWhichLoss = 'largest';
+  var chooseWhichGain = 'largest' ;
+  var howManyToPull = 1;
+  lossGainDict = convertToLossGain(ltt, 
+                                      'arrayLandTrendr',
+                                      lossMagThresh,
+                                      lossSlopeThresh,
+                                      gainMagThresh,
+                                      gainSlopeThresh,
+                                      slowLossDurationThresh,
+                                      chooseWhichLoss, 
+                                      chooseWhichGain, 
+                                      howManyToPull);
+  lossGainStack = LTLossGainExportPrep(lossGainDict,indexName = bandName,multBy = 1);
+  addLossGainToMap(lossGainStack,startYear,endYear,lossMagThresh-0.7,lossMagThresh,gainMagThresh,gainMagThresh+0.7);
+});
+// Vizualize image collection for charting (opacity set to 0 so it will chart but not be visible)
+Map.addLayer(lt_fit,{},'LT Fit TS');
+Map.setOptions('HYBRID');
